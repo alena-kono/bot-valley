216d4effe8e6b5ab643c3cecd28acbdd538ff94b
Alena Konovalova <konovalova.alena@gmail.com>
2021-07-30 18:18:10 +0300

    Implement view that outputs a list of all telegram users that use the bot

--- apps/telegram_bot/urls.py
+++ apps/telegram_bot/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from apps.telegram_bot.views import WebhookBotView
+from apps.telegram_bot.views import bot_users_view, webhook_bot_view
 from config.settings.base import TELEGRAM_API_TOKEN
 
 app_name = "telegram_bot"
 urlpatterns = [
-    path(
-        f"{TELEGRAM_API_TOKEN}", view=csrf_exempt(WebhookBotView.as_view()), name="hook"
-    ),
+    path(f"{TELEGRAM_API_TOKEN}", view=csrf_exempt(webhook_bot_view), name="webhook"),
+    path("users/", view=bot_users_view, name="bot users"),
 ]
--- apps/telegram_bot/views.py
+++ apps/telegram_bot/views.py
@@ -1,10 +1,12 @@
 import json
 import logging
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
-from django.views import View
+from django.views.generic.base import TemplateView, View
 
 from apps.telegram_bot.bot.webhook import process_telegram_event
+from apps.telegram_bot.models import TelegramUser
 
 
 class WebhookBotView(View):
@@ -13,5 +15,25 @@ class WebhookBotView(View):
     def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         request_body = json.loads(request.body)
         process_telegram_event(request_body)
-        logging.info(request_body)
+        logging.info(f"Telegram webhook: {request_body}")
         return HttpResponse({"ok": "POST request processed"}, status_code=200)
+
+
+webhook_bot_view = WebhookBotView.as_view()
+
+
+class BotUsersView(LoginRequiredMixin, TemplateView):
+    """Template view that outputs a list of all telegram users that use the bot.
+    Login is required to access this view.
+    """
+
+    template_name = "bot_users.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["users"] = TelegramUser.objects.all()
+        context["title"] = "Bot users"
+        return context
+
+
+bot_users_view = BotUsersView.as_view()
