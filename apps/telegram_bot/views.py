106043409970af5a8ef9433f644936045db3ef9e
Alena Konovalova <konovalova.alena@gmail.com>
2021-07-30 22:54:00 +0300

    Remove telegram user table from the site

--- apps/telegram_bot/views.py
+++ apps/telegram_bot/views.py
@@ -1,12 +1,10 @@
 import json
 import logging
 
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
-from django.views.generic.base import TemplateView, View
+from django.views.generic.base import View
 
 from apps.telegram_bot.bot.webhook import process_telegram_event
-from apps.telegram_bot.models import TelegramUser
 
 
 class WebhookBotView(View):
@@ -20,20 +18,3 @@ class WebhookBotView(View):
 
 
 webhook_bot_view = WebhookBotView.as_view()
-
-
-class BotUsersView(LoginRequiredMixin, TemplateView):
-    """Template view that outputs a list of all telegram users that use the bot.
-    Login is required to access this view.
-    """
-
-    template_name = "bot_users.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["users"] = TelegramUser.objects.all()
-        context["title"] = "Bot users"
-        return context
-
-
-bot_users_view = BotUsersView.as_view()
