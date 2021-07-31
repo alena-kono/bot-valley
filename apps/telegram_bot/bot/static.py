599c514ad5e713628990c7f042da400a9cfbc82a
Alena Konovalova <konovalova.alena@gmail.com>
2021-07-31 14:22:16 +0300

    Implement keyboard that allows to get current exchange rates for BTC, ETH, DOGE

---
+++ apps/telegram_bot/bot/static.py
@@ -0,0 +1,9 @@
+MESSAGES = {
+    "start": "Hello! I'm a bot which is created for the purpose of @BotValley test task.\
+However, I have one useful feature - I can show you the exchange rate (to USD) of the cryptocurrencies below.\
+Press one of these buttons and I'll do the rest!",
+    "crypto_exchange_rate_error": "Sorry, but it is impossible to get crypto exchange rates now.\
+Please, contact administrator @alena_kono for help.",
+    "any_other_content": "I can only send you current exchange rates for the currencies shown on a\
+keyboard below. Try and push the button!",
+}
